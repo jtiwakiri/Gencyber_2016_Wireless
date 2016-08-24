c1e93c076c32eda82a66a39ca160419fc3e79128
jtiwakiri <jtiwakiri@gmail.com>
2016-08-24 15:46:20 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -26,3 +26,7 @@ echo $point
 #echo $start
 
 #ap=$(expr substr $ap
+
+#http://tldp.org/LDP/Bash-Beginners-Guide/html/index.html
+#http://tldp.org/LDP/abs/html/
+#http://www.panix.com/~elflord/unix/bash-tute.html
