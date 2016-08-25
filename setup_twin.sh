cebc38c1b1cc6140fdba911e44367ba9bc7467c5
jtiwakiri <jtiwakiri@gmail.com>
2016-08-24 23:00:13 -0700

    Update setup_twin.sh

--- setup_twin.sh
+++ setup_twin.sh
@@ -17,6 +17,11 @@ sudo mv /etc/network/interfaces /etc/network/interfaces2
   sudo echo "iface wlan1 inet manual"
 } > /etc/network/interfaces
 
+# Set the run_twin script to run at boot
+sudo chmod 755 run_twin.sh
+sudo mv run_twin.sh /etc/init.d/run_twin.sh
+sudo update-rc.d -f lightdm remove
+sudo update-rc.d run_twin.sh defaults
 
 # Reboot the pi
 sudo reboot
